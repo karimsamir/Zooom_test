0ffe21a2838fc617a5ca77d0fb2e22d29958b688
karimsamir <karim5977@gmail.com>
2016-06-26 13:39:33 +0200

    change map.js file path

---
+++ public/admin/js/map.js
@@ -0,0 +1,129 @@
+var map = null;
+var marker = null;
+var infowindow = null;
+var geocoder = null;
+
+function initMap(lat, lng, container_div) {
+
+    var div_map = container_div.find(".map_canvas")[0];
+    var latlng = new google.maps.LatLng(parseFloat(lat), parseFloat(lng));
+
+    var mapOptions = {
+        zoom: 13,
+        center: latlng
+    }
+
+    map = new google.maps.Map(div_map, mapOptions);
+
+    var input = container_div.find("input[name=location]")[0];
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.bindTo('bounds', map);
+
+    infowindow = new google.maps.InfoWindow();
+
+    marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29),
+        draggable: true
+    });
+
+    geocoder = new google.maps.Geocoder;
+
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+
+        placeChanged(place, container_div, false);
+    });
+
+    // marker dragged event
+    marker.addListener('dragend', function () {
+        geocodeLatLng(marker.getPosition(), container_div);
+    });
+}
+
+function geocodeLatLng(latlng, container_div) {
+
+    geocoder.geocode({'location': latlng}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+
+                placeChanged(results[0], container_div, true);
+            } else {
+                window.alert('No results found');
+            }
+        } else {
+            window.alert('Geocoder failed due to: ' + status);
+        }
+    });
+}
+
+function placeChanged(place, container_div, marker_dragged) {
+    if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+    }
+
+    var address = '';
+    if (place.address_components) {
+        address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+    }
+
+    // if marker dragged change address in location input field
+    if (marker_dragged) {
+        container_div.find("input[name=location]").val(place.formatted_address);
+    }
+
+    //get address components
+    var address_components = place.address_components;
+    // set zip code in form
+    for (i = 0; i < address_components.length; i++) {
+        if (address_components[i]["types"] == "postal_code") {
+            container_div.find("input[name=zip]").val(address_components[i]["long_name"]);
+        }
+    }
+
+    // change lat lng in form
+    container_div.find("input[name=latitude]").val(place.geometry.location.lat());
+    container_div.find("input[name=longitude]").val(place.geometry.location.lng());
+
+    // show place marker
+    var place_name = "";
+    if (!marker_dragged) {
+        place_name = '<div><strong>' + place.name + '</strong><br>';
+    }
+
+    if (!marker_dragged) {
+        marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+    }
+
+    // change marker position
+    marker.setPosition(place.geometry.location);
+    // show marker
+    marker.setVisible(true);
+    // show info window
+    infowindow.setContent(place_name + address);
+    infowindow.open(map, marker);
+}
---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Zooom Test</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.css">
+    \
+    
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <link rel="stylesheet" type="text/css" id="theme" href="{{ asset('/css/style.css') }}"/>
+    
+    
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Laravel
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+        
+    <div id="alert_messages">
+        <div class="alert alert-success" style="margin-top:1em; display:none;">
+            <button type="button" class="close" data-dismiss="alert">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+            <span>
+                <strong class="content"></strong> 
+            </span>
+        </div>
+        <div class="alert alert-danger" style="margin-top:1em; display:none;">
+            <button type="button" class="close" data-dismiss="alert">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+            <span class="content"></span>
+        </div>
+    </div>
+    
+    @yield('content')
+
+    <!-- JavaScripts -->
+    <script src="//code.jquery.com/jquery-2.2.3.min.js"></script>    
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js" ></script>    
+    
+    
+    
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8cZEQ4NUtn43xVU8eZa-xQtGlo1t3p9Y&libraries=places"
+        async defer></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script> -->
+    
+    
+<!--    <script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+     <script type="text/javascript" src="{{ asset('bower_components/jquery-ui/jquery-ui.min.js')}}"></script>-->
+    
+    
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+@stack('scripts')
+</body>
+</html>
